672571c0901feba7938dc9e6e13ab858a1aa853e
quan <casanova.01042022@example.com>
2025-02-10 15:35:09 +0700

    First commit

---
+++ RUM_ra_jv240711_md4_ss01_springboot/src/main/java/com/ra/RumRaJv240711Md4Ss01SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumRaJv240711Md4Ss01SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RumRaJv240711Md4Ss01SpringbootApplication.class, args);
+    }
+
+}
